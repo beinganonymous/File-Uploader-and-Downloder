ec37991de88e0acfd79c557fcd2de7966c74c868
Chetan Patil <chetanpatil49554@gmail.com>
2019-04-12 23:40:32 +0530

    Add files via upload

---
+++ display_file.php
@@ -0,0 +1,11 @@
+<table style="border-collapse: collapse; font: 12px Tahoma;" border="1" cellspacing="5" cellpadding="5">
+<tbody><tr>
+<td>
+<embed src="<?php echo $row["image_name"]; ?>" width="800px" height="2100px" />
+
+<!--img src="<?php echo $row["images_path"]; ?>" alt="Image" width="200" height="200"-->
+
+
+</td>
+</tr>
+</tbody></table>
